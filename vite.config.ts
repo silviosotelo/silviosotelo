94a7322a1e331d4281708640ff3ee6c23fd32445
Silvio Sotelo <silvio.sotelo@hotmail.com>
2025-02-08 10:54:29 -0300

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Add this line to fix routing in production
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
